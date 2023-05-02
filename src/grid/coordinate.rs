257c626bc3b91b3e6f3e835d4096c28542200d2d
Nicolas Boisvert <nicklay@me.com>
2023-05-02 17:10:55 -0400

    Add basic tests

--- src/grid/coordinate.rs
+++ src/grid/coordinate.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use crate::grid::utils::parse_i8_tuple;
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Coordinate {
     pub row_index: i8,
     pub column_index: i8
@@ -21,5 +21,25 @@ impl Coordinate {
     pub fn parse(input: String) -> Option<Coordinate> {
         parse_i8_tuple(input, ',')
             .map(|(row_index, column_index)| Coordinate::new(row_index, column_index))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let coordinate = Coordinate::new(5, 6);
+        assert_eq!(coordinate, Coordinate { row_index: 5, column_index: 6 })   
+    }
+
+    #[test]
+    fn parse_valid() {
+        assert_eq!(Coordinate::parse("4,5".to_string()), Some(Coordinate { row_index: 4, column_index: 5 }));
+        assert_eq!(Coordinate::parse("4,".to_string()), None);
+        assert_eq!(Coordinate::parse("4".to_string()), None);
+        assert_eq!(Coordinate::parse("".to_string()), None)
     }
 }
--- src/grid/size.rs
+++ src/grid/size.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use crate::grid::utils::parse_i8_tuple;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Size {
     pub width: i8,
     pub height: i8
@@ -21,5 +21,24 @@ impl Size {
 impl fmt::Display for Size {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(formatter, "<{}x{}>", self.width, self.height)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let size = Size::new(5, 6);
+        assert_eq!(size, Size { width: 5, height: 6 })   
+    }
+
+    #[test]
+    fn parse() {
+        assert_eq!(Size::parse("4x5".to_string()), Some(Size { width: 4, height: 5 }));
+        assert_eq!(Size::parse("4,".to_string()), None);
+        assert_eq!(Size::parse("4".to_string()), None);
+        assert_eq!(Size::parse("".to_string()), None)
     }
 }
